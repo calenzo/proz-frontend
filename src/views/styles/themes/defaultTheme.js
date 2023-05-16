edd65bb5d3d39ae218bdfe033b9fbeb13b3a8ff3
Calenzo <calenzodevs@gmail.com>
2023-05-16 19:16:58 -0300

    feat: I has been done created the base of project

---
+++ src/views/styles/themes/defaultTheme.js
@@ -0,0 +1,15 @@
+export const Theme = {
+  media: {
+    mobile_portrait: "320px",
+    mobile_default: "360px",
+    mobile_landscape: "480px",
+    tablet_portrait: "600px",
+    tablet: "768px",
+    tablet_landscape: "801px",
+    laptops_landscape: "1024px",
+    desktop_hd: "1280px",
+    desktop_fullhd: "1600px",
+  },
+  fontSize: {},
+  colors: {},
+};
