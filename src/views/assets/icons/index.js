0ed1430282cea4366ee03b62c02c3a28f8dd163d
Calenzo <calenzodevs@gmail.com>
2023-05-16 21:05:58 -0300

    feat: I was implemented the icons to use in project

---
+++ src/views/assets/icons/index.js
@@ -0,0 +1,17 @@
+import Avatar from "views/assets/icons/avatar.svg";
+import Letter from "views/assets/icons/letter.svg";
+import Logo from "views/assets/icons/logo.svg";
+import Menu from "views/assets/icons/menu.svg";
+import NotificationOutline from "views/assets/icons/notification-outline.svg";
+import Notification from "views/assets/icons/notification.svg";
+import Warning from "views/assets/icons/warning.svg";
+
+export const Icons = {
+  Avatar,
+  Letter,
+  Logo,
+  Menu,
+  NotificationOutline,
+  Notification,
+  Warning,
+};
