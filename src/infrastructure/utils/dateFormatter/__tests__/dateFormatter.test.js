64d5e2e94a67a67cfe064405721ef8df293dcaa0
Calenzo <calenzodevs@gmail.com>
2023-05-18 09:42:17 -0300

    feat: I was updated the function of test to pattern

--- src/infrastructure/utils/dateFormatter/__tests__/dateFormatter.test.js
+++ src/infrastructure/utils/dateFormatter/__tests__/dateFormatter.test.js
@@ -8,41 +8,41 @@ const resultFormatter = {
 };
 
 describe("Date formatter", () => {
-  it("should to return empty string when try to formatter with null value.", () => {
+  test("should to return empty string when try to formatter with null value.", () => {
     expect(dateFormatter()).toEqual("");
   });
 
-  it("should to return error when try to formatter a string value.", () => {
+  test("should to return error when try to formatter a string value.", () => {
     expect(() => dateFormatter("Proz Educação")).toThrowError(
       "Invalid time value"
     );
   });
 
-  it("should to return error when try to formatter a invalid date value.", () => {
+  test("should to return error when try to formatter a invalid date value.", () => {
     expect(() => dateFormatter("2021-03-32")).toThrowError(
       "Invalid time value"
     );
   });
 
-  it("should to formatter date iso with pattern dd/MMMM/YYYY.", () => {
+  test("should to formatter date iso with pattern dd/MMMM/YYYY.", () => {
     const formatedValue = dateFormatter(dateMock.iso);
 
     expect(formatedValue).toEqual(resultFormatter.default);
   });
 
-  it("should to formatter string valid date to pattern dd-MMMM-YYYY (with less caracter),", () => {
+  test("should to formatter string valid date to pattern dd-MMMM-YYYY (with less caracter),", () => {
     const formatedValue = dateFormatter(dateMock.iso, "dd-MM-yyyy");
 
     expect(formatedValue).toEqual(resultFormatter.withLessPattern);
   });
 
-  it("should to formatter string valid date to pattern dd/MMMM/YYYY.", () => {
+  test("should to formatter string valid date to pattern dd/MMMM/YYYY.", () => {
     const formatedValue = dateFormatter("2023-05-17");
 
     expect(formatedValue).toEqual("17/05/2023");
   });
 
-  it("should to formatter object date valid date to pattern dd/MMMM/YYYY.", () => {
+  test("should to formatter object date valid date to pattern dd/MMMM/YYYY.", () => {
     const formatedValue = dateFormatter(dateMock.object);
 
     expect(formatedValue).toEqual(resultFormatter.default);
