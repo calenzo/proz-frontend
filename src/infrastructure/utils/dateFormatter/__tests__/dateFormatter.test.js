b6a2485d692f4698d2c61943c56cf85ad94b844c
Calenzo <calenzodevs@gmail.com>
2023-05-17 23:31:38 -0300

    feat: I was implemented the test of dateFormatter

---
+++ src/infrastructure/utils/dateFormatter/__tests__/dateFormatter.test.js
@@ -0,0 +1,44 @@
+import { dateFormatter } from "..";
+
+import { dateMock } from "../_mock";
+
+const resultFormatter = {
+  default: `${dateMock.day}/${dateMock.month}/${dateMock.year}`,
+  withLessPattern: `${dateMock.day}-${dateMock.month}-${dateMock.year}`,
+};
+
+describe("Date formatter", () => {
+  it("should to return empty string when try to formatter with null value.", () => {
+    expect(dateFormatter()).toEqual("");
+  });
+
+  it("should to return error when try to formatter a string value.", () => {
+    expect(() => dateFormatter("Proz Educação")).toThrowError(
+      "Invalid time value"
+    );
+  });
+
+  it("should to return error when try to formatter a invalid date value.", () => {
+    expect(() => dateFormatter("2021-03-32")).toThrowError(
+      "Invalid time value"
+    );
+  });
+
+  it("should to formatter date iso with pattern dd/MMMM/YYYY.", () => {
+    const formatedValue = dateFormatter(dateMock.iso);
+
+    expect(formatedValue).toEqual(resultFormatter.default);
+  });
+
+  it("should to formatter string valid date to pattern dd-MMMM-YYYY (with less caracter),", () => {
+    const formatedValue = dateFormatter(dateMock.iso, "dd-MM-yyyy");
+
+    expect(formatedValue).toEqual(resultFormatter.withLessPattern);
+  });
+
+  it("should to formatter string valid date to pattern dd/MMMM/YYYY.", () => {
+    const formatedValue = dateFormatter("2023-05-17");
+
+    expect(formatedValue).toEqual("17/05/2023");
+  });
+});
---
+++ src/infrastructure/utils/dateFormatter/_mock.js
@@ -0,0 +1,14 @@
+const dateNow = new Date();
+
+export const dateMock = {
+  iso: dateNow.toISOString(),
+  hours: (dateNow.getHours() / 100).toFixed(2).toString().slice(2),
+  min: (dateNow.getMinutes() / 100).toFixed(2).toString().slice(2),
+  seconds: (dateNow.getSeconds() / 100).toFixed(2).toString().slice(2),
+  day:
+    dateNow.getDate() < 10
+      ? (dateNow.getDate() / 100).toFixed(2).toString().slice(2)
+      : dateNow.getDate(),
+  month: ((dateNow.getMonth() + 1) / 100).toFixed(2).toString().slice(2),
+  year: dateNow.getFullYear(),
+};
